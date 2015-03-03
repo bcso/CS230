e4f188140cbba8e3a97e46d3bd7fbd7ab0aecdcc
bcso <bcso@student.cs.uwaterloo.ca>
2015-03-03 16:33:21 -0500

    completed q3

--- Assignment 3/a3q3.asm
+++ Assignment 3/a3q3.asm
@@ -1,33 +1,54 @@
-;Registers:
+; Perform insertion sort on an array.
+; Registers:
 ;	$4 - final result
 ;	$5 - holds array incrementer, i for outer loop
 ;	$8 - length of array
-;	$9 - copy of unsorted array ($1) for use in inner loop
+;	$9 - copy start address of $1 for resetting after inserting
 ;	$10 - slt bit store
-;	$11 - value of interest to be swapped, retrieved from current i position
-;	$12 - holds original value of $31
+;	$12 - holds value of 1
+;	$14 - temp value store
+;	$15 - temp value store
+;	$16 - holds original value of $31
+;	$25 - temp value store
 
-		lis $6
-		.word 0xFFFF000C
-		addi $8, $2, 0		
-		addi $4, $1, 0		
-		addi $5, $1, 0		
-		addi $5, $5, 4		 
-		addi $9, $1, 0		 
-		addi $10, $0, 0	
-		addi $11, $0, 0		
-		addi $12, $31, 0	
+		addi $8, $0, 4		
+		mult $8, $2
+		mflo $8			
+		add  $8, $8, $1		; end position at $8
+
+		addi $4, $1, 0		; save array start pos to $4
+		addi $5, $1, 0		; save array start pos to $5
+		addi $5, $5, 4		; have $5 counter start one value ahead, since first value is sorted
+		addi $9, $1, 0		; save array start pos to $9 
+		addi $10, $0, 0		; init slt bit store	
+		addi $12, $0, 1		; init 12, used for slt comparison
+		addi $16, $31, 0	; save original return address	
 		beq $0, $0, oloop
 
-oloop:		lw $21, 0($5)		; store value at curpos of oloop to $21 (value to insert into sorted array)
+oloop:		lw $15, 0($5)		; store value at curpos of oloop to $21 (value to insert into sorted array)
 		beq $5, $8, end		; end program, finished iterating through all i
+		addi $4, $9, 0		; reset the index of sorted array, for insertion	
 		jal insert		; go to insert $21 into sorted array
-		addi $5, $5, 4		; increment $5 by 1
+		addi $5, $5, 4		; increment outer loop
+		beq $5, $8, end 	; end program, finished interating through all i
+		bne $5, $8, oloop	; go loop again
 
-insert:		lw $22, 0($4)		; save value of sorted array
-		  
+insert:		lw $14, 0($4)		; load value of sorted array
+		slt $10, $14, $15	; value in sorted array is less than new value of interest		
+		beq $10, $12, lessthan 	; go to lessthan case
+		bne $10, $12, greaterthan ; go to greaterthan case
 
-end:		addi $31, $12, 0	; restore original value of $31
+lessthan:	addi $4, $4, 4		; increment inserting check pointer
+		bne $4, $5, insert 	; go back to insert to load next value from sorted array to check for insert
+		jr $31			; reach end of sorted array, go back to olooop
+
+greaterthan:	addi $25, $15, 0	; add $15 to temp val	  
+		addi $15, $14, 0	; move value $14 to $15
+		addi $14, $25, 0	; move prev value of $15 into $25
+		sw $14, 0($4)
+		addi $4, $4, 4
+		sw $15, 0($4)
+		jr $31			; finished inserting current value, go back to oloop
+
+end:		addi $31, $16, 0	; restore original value of $31
 		jr $31
-
-jr $31
