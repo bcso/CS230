ccf4061637c4256c85f706762aed0379b8995faa
bcso <bcso@uwaterloo.ca>
2015-03-03 13:20:37 -0500

    changed argument register names

--- Assignment 3/a3q1.asm
+++ Assignment 3/a3q1.asm
@@ -1,7 +1,7 @@
 ;; This program takes in three user inputs, two integers to each define a start and stop index respectively
 ;; and a string which will be printed as a substring according to the two integers the user has defined.
-		add $8, $1, $0 ; start index
-		add $9, $2, $0 ; stop index
+		add $4, $1, $0 ; start index
+		add $5, $2, $0 ; stop index
 		addi $10, $0, 1 ; save 1
 		addi $11, $0, 0 ; init the counter
 
@@ -10,7 +10,7 @@
 		lis $15		; write to screen
 		.word 0xFFFF000C
 
-iterate:	beq $11, $8, print	; Jump to print when finished loading up to start index
+iterate:	beq $11, $4, print	; Jump to print when finished loading up to start index
 		add $11, $11, $10		; Increment counter by 1
 		lw $13, 0($14)		; load the next character
 		beq $0, $0, iterate	; iterate again
@@ -18,7 +18,7 @@ iterate:	beq $11, $8, print	; Jump to print when finished loading up to start in
 print:		lw $13, 0($14)		; load character from $14 to $13
 		sw $13, 0($15)		; load character from $13 to output (screen)
 		add $11, $11, $10		; Increment counter by 1
-		bne $11, $9, print	; iterate again
+		bne $11, $5, print	; iterate again
 
 		addi $13, $0, 0xB	; load the new line character into register $13
 		sw $13, 0($15)		; print the new line charcter
